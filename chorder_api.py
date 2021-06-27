65d1c013dabbaa45d529f15a0fbe729ecd4b0586
jgoldberg <jgoldberg@consumer-edge.com>
2021-06-27 13:17:49 -0400

    initial commit of chorder api

---
+++ chorder_api.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from chorder import key_finder
+
+
+chorder = FastAPI()
+
+@chorder.get("/chorder/")
+async def read_item(root_note,scale='major'):
+
+    ret_dict = {}
+    try:
+
+        init_dict = key_finder(root_note, scale)
+        init_key = list(init_dict.keys())[0]
+
+        chord_list = init_dict[init_key]
+
+        ret_key = init_key.replace("Chords of ", "")
+
+        ret_dict[ret_key] = chord_list
+
+        ret_dict['success'] = True
+
+    except Exception as e:
+        ret_dict['success'] = False
+        ret_dict['error'] = e
+
+
+    return ret_dict
