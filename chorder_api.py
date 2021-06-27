abecf7e0cdc5009373711948f45f6e6489e505ca
jgoldberg <jgoldberg@consumer-edge.com>
2021-06-27 17:35:33 -0400

    updated exception status code

--- chorder_api.py
+++ chorder_api.py
@@ -1,12 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Response, status
 from chorder import key_finder
-from fastapi import Query
 
 
 chorder = FastAPI(title="Chorder", description="Retrieve the chords of a key")
 
 @chorder.get("/chorder/")
-async def read_item(root_note: str=Query(..., description="root note of the scale e.g. 'C'"),
+async def read_item(response: Response, root_note: str=Query(..., description="root note of the scale e.g. 'C'"), 
                     scale: str=Query(default="major")):
 
     ret_dict = {}
@@ -26,6 +25,7 @@ async def read_item(root_note: str=Query(..., description="root note of the scal
     except Exception as e:
         ret_dict['success'] = False
         ret_dict['error'] = str(e)
+        response.status_code = status.HTTP_400_BAD_REQUEST
 
 
     return ret_dict
